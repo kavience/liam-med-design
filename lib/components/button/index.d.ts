65104cb534e0ad1ddbd4ca2a0f6251368caff1de
kavience <599513860@qq.com>
2020-05-30 14:40:04 +0800

    feat(ts): 新增 ts 类型

---
+++ lib/components/button/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="react" />
+import { ButtonProps } from 'antd/lib/button';
+declare const _default: (props: ButtonProps) => JSX.Element;
+export default _default;
---
+++ lib/components/helper/demo-container/index.d.ts
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.less';
+interface IProps {
+    className?: any;
+    moreHref?: string;
+}
+export declare class DemoContainer extends React.Component<IProps> {
+    render(): JSX.Element;
+}
+export declare class DemoPanelContainer extends React.Component<IProps> {
+    render(): JSX.Element;
+}
+export {};
