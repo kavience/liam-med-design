821dab75218af8c46edbcb3da293a9945a1ce2c6
kavience <599513860@qq.com>
2020-06-17 11:10:47 +0800

    feat(更新icon): 更新icon

--- src/components/icon/index.tsx
+++ src/components/icon/index.tsx
@@ -1,7 +1,7 @@
 import { createFromIconfontCN } from '@ant-design/icons';
 
 export const CustomIcon = createFromIconfontCN({
-  scriptUrl: '//at.alicdn.com/t/font_1661400_7dynpucgsk6.js',
+  scriptUrl: '//at.alicdn.com/t/font_1661400_o6ho0h4def.js',
 });
 
 export * from '@ant-design/icons';
